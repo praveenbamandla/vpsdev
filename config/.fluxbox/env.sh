9201ea6bc7689215356bc2a21dc16b67b36e24d1
Praveen <praveenbamandla@gmail.com>
2015-01-02 18:52:28 +0530

    Added environment set script for terminal and xfe config file

---
+++ config/.fluxbox/env.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+export SHELL=/bin/sh
+export USER=root
+export NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript
+# 
+/bin/bash
+
